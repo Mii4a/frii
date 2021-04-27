112678174428c6356cd244ae5ed908a74aac733e
mii4a <beppu0918@outlook.jp>
2021-04-27 21:10:40 +0900

    Revisionized User

--- backend/spec/requests/users_request_spec.rb
+++ backend/spec/requests/users_request_spec.rb
@@ -9,22 +9,4 @@ RSpec.describe "Users", type: :request do
       expect(response).to have_http_status(200)
     end
   end
-
-  describe 'POST /api/v1/users' do
-    context 'with valid params'  do
-      let(:params) do
-        { user: {
-          name: 'test_user',
-          email: 'test@user.com',
-          password: 'password',
-          password_confirmation: 'password'
-        }}
-      end
-      it 'succeeds to create a new user' do
-        expect do
-          post api_v1_user_registration_path, params: params
-        end.to change(User, :count).by(1)
-      end
-    end
-  end
 end
